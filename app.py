a85ea6213998cb8c8703d3f5ddef1117e28ffef1
monsebolena <monsebaldo7@gmail.com>
2025-03-11 20:28:06 +0100

    registro probar

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request,redirect,url_for,session
+import pymysql
+import db
+
+
+
+
+app = Flask(__name__)
+app.secret_key="123456"
+
+
+    
+@app.route('/registro-cliente',methods=['GET'])
+def registroCliente():
+    return render_template("registro-cliente.html")
+   
+
+if __name__ == '__main__':    
+    app.run(debug=True,port=80)
