f18f753f94e13cbf0a02baa8c586500db8df75e4
monsebolena <monsebaldo7@gmail.com>
2025-03-12 17:11:37 +0100

    registro ok

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pymysql
 import db
 
 
-
-
 app = Flask(__name__)
 app.secret_key="123456"
 
@@ -12,8 +10,14 @@ app.secret_key="123456"
     
 @app.route('/registro-cliente',methods=['GET'])
 def registroCliente():
-    return render_template("registro-cliente.html")
-   
+    return render_template("registro/registro-cliente.html")
 
+@app.route('/registro-restaurante',methods=['GET'])
+def registroRestaurante():
+    return render_template("registro/registro-restaurante.html")
+
+   
 if __name__ == '__main__':    
     app.run(debug=True,port=80)
+    
+
